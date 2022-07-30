0c767aaf1b1ac9ead600c329c7309beaca8f3f4d
wlllllllk <64308668+wlllllllk@users.noreply.github.com>
2022-07-30 15:35:53 +0800

    error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,15 +1,29 @@
 import "./style.scss";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const navigate = useNavigate();
+
   return (
     <div className="error">
-      <h1>Error 404!</h1>
-      <h1>Page doesn't exist!</h1>
+      <div className="container">
+        <span></span>
+        {/* <img src="/src/icons/not_found.svg" alt="" /> */}
+        <h1>Sorry!</h1>
+        <h1>The page you are looking for does not exist.</h1>
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Continue shopping
+        </button>
+      </div>
     </div>
   );
 };
